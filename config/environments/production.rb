d45087267c13742b0b7490f67f10e93967e14505
YUTA MIZUI <futebol.de.salao10@gmail.com>
2022-10-08 12:58:14 +0900

    FIxing sendgrid error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'apikey',
-    :password       => ENV['SENDGRID_API_KEY'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'rizatta.com',
     :enable_starttls_auto => true
   }
