2910bdb0fadfa37e5537035498f6b39e4d4a331a
YUTA MIZUI <futebol.de.salao10@gmail.com>
2022-10-27 20:25:55 +0900

    Updated layout

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,32 +7,19 @@ class TicketsController < ApplicationController
 
   def new
     @ticket = Ticket.new
-    @ticket.branch_id = params[:branch_id]
+    @branch = Branch.find(params[:branch_id])
+    @user = User.find(params[:user_id])
   end
 
   def create
     params[:number_of_ticket].to_i.times do
       @ticket = Ticket.create(
-        expired_at: params[:expired_at].in_time_zone.end_of_day,
-        user_id: params[:user_id],
+        expired_at: params[:ticket][:expired_at].in_time_zone.end_of_day,
+        user_id: params[:ticket][:user_id],
         status: "true",
       )
-      
     end
     redirect_to users_path(branch_id: @ticket.user.branch_id), notice: t('activerecord.attributes.link.created')
-  end
-
-  def edit
-  end
-
-  def update
-    @ticket.update(ticket_params)
-    if ticket.update(ticket_params)
-      redirect_to ticket_path(id: @ticket.id), notice: t('activerecord.attributes.link.edited')
-    else
-      flash[:notice] = t('activerecord.attributes.link.failed_to_create')
-      render 'tickets/edit'
-    end
   end
 
   def destroy
